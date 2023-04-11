e01b7999e228b6ade96d21ecf78544f69efaba0a
Viren070 <viren_2017@hotmail.com>
2023-04-11 13:16:22 +0000

    reorganise code and move into one file

--- pdfscraper.py
+++ pdfscraper.py
@@ -1,8 +1,10 @@
+import errno
 import os
 import re
+import sys
 import threading
-import tkinter
 import time
+import tkinter
 from pathlib import Path
 from tkinter import filedialog, messagebox
 from urllib.parse import urljoin
@@ -11,8 +13,7 @@ import customtkinter
 import requests
 from bs4 import BeautifulSoup
 
-import path_validator as PV
-
+ERROR_INVALID_NAME = 123
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -49,8 +50,8 @@ class App(customtkinter.CTk):
         self.upper_range_entry = customtkinter.CTkEntry(self.range_frame, width=50)
         self.upper_range_entry.grid(row=0, column=2, padx=10, pady=10)
 
-        self.find_button = customtkinter.CTkButton(self, text="Search URL for PDFs", command=self.find_url_button_event)
-        self.find_button.grid(row=4, column=0, padx=10, pady=10)
+        self.search_button = customtkinter.CTkButton(self, text="Search URL for PDFs", command=self.search_url_button_event)
+        self.search_button.grid(row=4, column=0, padx=10, pady=10)
         
         self.download_button = customtkinter.CTkButton(self, text="Download", command=self.download_button_event)
         self.download_button.grid(row=4, column=1, padx=10, pady=10)
@@ -73,28 +74,7 @@ class App(customtkinter.CTk):
             return False
         else:
             return True
-    def find_url_button_event(self, download_next=False):
-        url=self.url_entry.get()
-        self.download_button.configure(state="disabled")
-        self.find_button.configure(state="disabled")
-        self.url_entry.configure(state="disabled")
-        self.lower_range_entry.configure(state="disabled")
-        self.upper_range_entry.configure(state="disabled")
-        if not self.is_url(url):
-            self.download_button.configure(state="normal")
-            self.find_button.configure(state="normal")
-            self.lower_range_entry.configure(state="normal")
-            self.upper_range_entry.configure(state="normal")
-            self.url_entry.configure(state="normal")
-            return 
-
-        self.new_thread=threading.Thread(target=self.find_urls)
-        self.new_thread.start()
-        if download_next:
-            self.result_thread=threading.Thread(target=self.check_result, args=(url,))
-            self.result_thread.start()
-            self.waiting_for_result=True
-            
+        
     def check_result(self, url):
         print("started check result thread")
         count=1
@@ -112,9 +92,20 @@ class App(customtkinter.CTk):
             print("starting download button event")
             self.download_button_event()
        
+    def search_url_button_event(self, download_next=False):
+        url=self.url_entry.get()
+        
+        if not self.is_url(url):
             
-     
-            
+            return 
+        self.change_widget_state("disabled")
+        self.new_thread=threading.Thread(target=self.find_urls)
+        self.new_thread.start()
+        if download_next:
+            self.result_thread=threading.Thread(target=self.check_result, args=(url,))
+            self.result_thread.start()
+            self.waiting_for_result=True
+                     
     def find_urls(self):
         url=self.url_entry.get()
         self.url = url
@@ -330,15 +321,136 @@ class App(customtkinter.CTk):
             self.output_text.insert(customtkinter.END, "{} / {} PDF files downloaded\n".format(success_count, len(self.pdf_links)))
         self.output_text.see("end")
         self.cancel_button.configure(state="disabled") 
-        self.download_button.configure(state="normal")
-        self.find_button.configure(state="normal") 
-        self.lower_range_entry.configure(state="normal")
-        self.upper_range_entry.configure(state="normal")
-        self.url_entry.configure(state="normal")
+
+    def cancel_download(self):
+        self.download_cancel_raised = True
+    def change_widget_state(self, state):
+        self.download_button.configure(state=state)
+        self.search_button.configure(state=state) 
+        self.lower_range_entry.configure(state=state)
+        self.upper_range_entry.configure(state=state)
+        self.url_entry.configure(state=state)
+
+    def select_folder(self):
+        folder_path = filedialog.askdirectory()
+        self.folder_entry.delete(0, customtkinter.END)
+        self.folder_entry.insert(0, folder_path)
+    
+    def valid_directory(self, directory):
+        if len(directory)==0:
+            if not messagebox.askokcancel("PDF Scraper","You have not specified a download location, the files will download in the current directory."):
+                return False
+            else:
+                self.folder_entry.delete(0, customtkinter.END)
+                self.folder_entry.insert(0, os.getcwd())
+        
+        elif not self.is_path_exists_or_creatable(directory):
+            self.output_text.insert(customtkinter.END, "Invalid Directory provided by user\n")
+            self.output_text.see("end")
+            return False
+        
+        return True 
+
+    def is_pathname_valid(self, pathname: str) -> bool:
+        '''
+        `True` if the passed pathname is a valid pathname for the current OS;
+        `False` otherwise.
+        '''
+        # If this pathname is either not a string or is but is empty, this pathname
+        # is invalid.
+        try:
+            if not isinstance(pathname, str) or not pathname:
+                return False
+
+            # Strip this pathname's Windows-specific drive specifier (e.g., `C:\`)
+            # if any. Since Windows prohibits path components from containing `:`
+            # characters, failing to strip this `:`-suffixed prefix would
+            # erroneously invalidate all valid absolute Windows pathnames.
+            _, pathname = os.path.splitdrive(pathname)
+
+            # Directory guaranteed to exist. If the current OS is Windows, this is
+            # the drive to which Windows was installed (e.g., the "%HOMEDRIVE%"
+            # environment variable); else, the typical root directory.
+            root_dirname = os.environ.get('HOMEDRIVE', 'C:') \
+                if sys.platform == 'win32' else os.path.sep
+            assert os.path.isdir(root_dirname)   # ...Murphy and her ironclad Law
+
+            # Append a path separator to this directory if needed.
+            root_dirname = root_dirname.rstrip(os.path.sep) + os.path.sep
+
+            # Test whether each path component split from this pathname is valid or
+            # not, ignoring non-existent and non-readable path components.
+            for pathname_part in pathname.split(os.path.sep):
+                try:
+                    os.lstat(root_dirname + pathname_part)
+                # If an OS-specific exception is raised, its error code
+                # indicates whether this pathname is valid or not. Unless this
+                # is the case, this exception implies an ignorable kernel or
+                # filesystem complaint (e.g., path not found or inaccessible).
+                #
+                # Only the following exceptions indicate invalid pathnames:
+                #
+                # * Instances of the Windows-specific "WindowsError" class
+                #   defining the "winerror" attribute whose value is
+                #   "ERROR_INVALID_NAME". Under Windows, "winerror" is more
+                #   fine-grained and hence useful than the generic "errno"
+                #   attribute. When a too-long pathname is passed, for example,
+                #   "errno" is "ENOENT" (i.e., no such file or directory) rather
+                #   than "ENAMETOOLONG" (i.e., file name too long).
+                # * Instances of the cross-platform "OSError" class defining the
+                #   generic "errno" attribute whose value is either:
+                #   * Under most POSIX-compatible OSes, "ENAMETOOLONG".
+                #   * Under some edge-case OSes (e.g., SunOS, *BSD), "ERANGE".
+                except OSError as exc:
+                    if hasattr(exc, 'winerror'):
+                        if exc.winerror == ERROR_INVALID_NAME:
+                            return False
+                    elif exc.errno in {errno.ENAMETOOLONG, errno.ERANGE}:
+                        return False
+        # If a "TypeError" exception was raised, it almost certainly has the
+        # error message "embedded NUL character" indicating an invalid pathname.
+        except TypeError as exc:
+            return False
+        # If no exception was raised, all path components and hence this
+        # pathname itself are valid. (Praise be to the curmudgeonly python.)
+        else:
+            return True
+        # If any other exception was raised, this is an unrelated fatal issue
+        # (e.g., a bug). Permit this exception to unwind the call stack.
+        #
+        # Did we mention this should be shipped with Python already?
+    def is_path_creatable(self, pathname: str) -> bool:
+        '''
+        `True` if the current user has sufficient permissions to create the passed
+        pathname; `False` otherwise.
+        '''
+        # Parent directory of the passed path. If empty, we substitute the current
+        # working directory (CWD) instead.
+        dirname = os.path.dirname(pathname) or os.getcwd()
+        return os.access(dirname, os.W_OK)
+    def is_path_exists_or_creatable(self, pathname: str) -> bool:
+        
+        '''
+        `True` if the passed pathname is a valid pathname for the current OS _and_
+        either currently exists or is hypothetically creatable; `False` otherwise.
+
+        This function is guaranteed to _never_ raise exceptions.
+        '''
+        
+        try:
+            # To prevent "os" module calls from raising undesirable exceptions on
+            # invalid pathnames, is_pathname_valid() is explicitly called first.
+            return self.is_pathname_valid(pathname)
+            return is_pathname_valid(pathname) and (
+                os.path.exists(pathname) or is_path_creatable(pathname))
+        # Report failure on non-fatal filesystem complaints (e.g., connection
+        # timeouts, permissions issues) implying this path to be inaccessible. All
+        # other exceptions are unrelated fatal issues and should not be caught here.
+        except OSError:
+            return False
+        
+
 #-------------------------------------------------------------
-
-
-
 
 
 PDFScraper = App()
