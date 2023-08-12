2363df8e292ac03f1f261c7b2f2c12b712c5bdc4
Viren070 <viren070@protonmail.com>
2023-08-12 19:03:17 +0100

    fix links for some websites

--- src/parser/pdf_parser.py
+++ src/parser/pdf_parser.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 from tkinter import messagebox
 import re
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
 
 
 def get_pdfs(url):      
@@ -19,5 +19,16 @@ def get_pdfs(url):
         if ('.pdf' in link.get('href', [])) and not(link.get('href', []) in [link[0] for link in pdf_links]):
             filename_two=re.sub('<[^>]+>', '', str(link))
             filename_one=link['href'].split('/')[-1].split('.pdf')[-2] 
-            pdf_links.append([urljoin(url,link.get('href')), filename_one, filename_two])
+            linkk = link.get('href').replace("\\","").replace('"','')
+            result=urlparse(linkk)
+            if all([result.scheme, result.netloc]):
+                pdf_url=linkk
+            else:
+                pdf_url =urljoin(url,link.get('href'))
+                result=urlparse(pdf_url)
+                if not all([result.scheme, result.netloc]):
+                    messagebox.showerror("Error", f"Received: {link.get('href')} \nwith urljoin: {pdf_url}\nResult = False")
+                    continue
+            print(pdf_url)
+            pdf_links.append([pdf_url, filename_one, filename_two])
     return pdf_links
