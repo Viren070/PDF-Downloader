38cd6d8a01ae8a0c0dde3aaf596d7a923065c9ce
Viren070 <viren070@protonmail.com>
2023-08-13 16:10:59 +0100

    stuff

--- src/main.py
+++ src/main.py
@@ -2,7 +2,3 @@ from gui.url_selector import URLSelector
 import os
 if __name__ == "__main__":
     url_selector = URLSelector(os.path.join(os.path.dirname(os.path.realpath(__file__)), "images"))
-
-#pyinstaller --noconfirm --onefile --windowed --name "PDF Downloader" --distpath ./dist/ --workpath ./build --add-data "C:/Users/viren/AppData/Local/Programs/Python/Python311/Lib/site-packages/customtkinter;customtkinter/" --add-data "C:\Users\viren\Documents\School\Computer Science\PDF Downloader\PDF-Downloader\src\images;images" "C:\Users\viren\Documents\School\Computer Science\PDF Downloader\PDF-Downloader\src\main.py"
-
-#d
