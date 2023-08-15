54d3b93cbb51f758d83b69fc05e92ab9b5683a02
Viren070 <viren070@protonmail.com>
2023-08-15 14:54:42 +0100

    add user agent

--- src/downloader/downloader.py
+++ src/downloader/downloader.py
@@ -39,6 +39,7 @@ class PDF:
             os.makedirs(download_folder)
     def create_session(self):
         headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36', # user agent 
             'Accept-Encoding': 'identity' # Disable compression
         }
 
